ea1697d7e8f4d6ed05540c77e7b0ac8e55858305
HyperGH <46067571+HyperGH@users.noreply.github.com>
2021-07-28 03:00:15 +0200

    Fix help

--- extensions/help.py
+++ extensions/help.py
@@ -37,11 +37,11 @@ class SnedHelp(commands.HelpCommand):
     See the docs or this guide (https://gist.github.com/InterStella0/b78488fb28cadf279dfd3164b9f0cf96) on how this was made.
     '''
     #Method to get information about a command to display in send_bot_help
-    def get_command_signature(self, command):
-        return '**`{prefix}{parent}{command}`** - {commandbrief}'.format(prefix=self.clean_prefix, parent=command.full_parent_name, command=command.name, commandbrief=command.short_doc) #short_doc goes to brief first, otherwise gets first line of help
+    def get_command_signature(self, ctx, command):
+        return '**`{prefix}{parent}{command}`** - {commandbrief}'.format(prefix=ctx.clean_prefix, parent=command.full_parent_name, command=command.name, commandbrief=command.short_doc) #short_doc goes to brief first, otherwise gets first line of help
     
-    def get_subcommand_signature(self, group, command): #Essentially the same as get_command_signature but appends the group name in front of the command
-        return '**`{prefix}{group} {command}`** - {commandbrief}'.format(prefix=self.clean_prefix, group=group.name, command=command.name, commandbrief=command.short_doc)
+    def get_subcommand_signature(self, ctx, group, command): #Essentially the same as get_command_signature but appends the group name in front of the command
+        return '**`{prefix}{group} {command}`** - {commandbrief}'.format(prefix=ctx.clean_prefix, group=group.name, command=command.name, commandbrief=command.short_doc)
     
     #Send generic help message with all commands included
     async def send_bot_help(self, mapping):
@@ -52,7 +52,7 @@ class SnedHelp(commands.HelpCommand):
         #We retrieve all the commands from the mapping of cog,commands
         for cog, commands in mapping.items(): 
             filtered = await self.filter_commands(commands, sort=True)   #This will filter commands to those the user can actually execute
-            command_signatures = [self.get_command_signature(command) for command in filtered]   #Get command signature in format as specified above
+            command_signatures = [self.get_command_signature(ctx, command) for command in filtered]   #Get command signature in format as specified above
             #If we have any, put them in categories according to cogs, fallback is "Other"
             if command_signatures:
                 cog_name = getattr(cog, "qualified_name", "Other") #Append items into a list of str, one item per cog
@@ -84,21 +84,21 @@ class SnedHelp(commands.HelpCommand):
         ctx = self.context   #Obtaining ctx
         self._ = ctx.bot.get_localization('help', ctx.bot.lang)
         if command.parents:
-            detail_embed=discord.Embed(title="⚙️ " + self._("Command: {prefix}{parent} {command}").format(prefix=self.clean_prefix, parent=command.full_parent_name, command=command.name), color=ctx.bot.embedBlue)
+            detail_embed=discord.Embed(title="⚙️ " + self._("Command: {prefix}{parent} {command}").format(prefix=ctx.clean_prefix, parent=command.full_parent_name, command=command.name), color=ctx.bot.embedBlue)
         else:
-            detail_embed=discord.Embed(title="⚙️ " + self._("Command: {prefix}{command}").format(prefix=self.clean_prefix, command=command.name), color=ctx.bot.embedBlue)
+            detail_embed=discord.Embed(title="⚙️ " + self._("Command: {prefix}{command}").format(prefix=ctx.clean_prefix, command=command.name), color=ctx.bot.embedBlue)
         if command.description:
             detail_embed.add_field(name=self._("Description:"), value=command.description)  #Getting command description
         elif command.help:
             detail_embed.add_field(name=self._("Description:"), value=command.help)  #Fallback to help attribute if description does not exist
         if command.usage:
-            detail_embed.add_field(name=self._("Usage:"), value=f"**`{self.clean_prefix}{command.usage}`**", inline=False) #Getting command usage & formatting it
+            detail_embed.add_field(name=self._("Usage:"), value=f"**`{ctx.clean_prefix}{command.usage}`**", inline=False) #Getting command usage & formatting it
         aliases = []
         for alias in command.aliases:
             if command.parents:
-                aliases.append(f"**`{self.clean_prefix}{command.full_parent_name} {alias}`**")
+                aliases.append(f"**`{ctx.clean_prefix}{command.full_parent_name} {alias}`**")
             else:
-                aliases.append(f"**`{self.clean_prefix}{alias}`**")  #Adding some custom formatting to each alias
+                aliases.append(f"**`{ctx.clean_prefix}{alias}`**")  #Adding some custom formatting to each alias
         if aliases:
             detail_embed.add_field(name=self._("Aliases:"), value=", ".join(aliases), inline=False)   #If any aliases exist, we add those to the embed in new field
         detail_embed.set_footer(text=ctx.bot.requestFooter.format(user_name=ctx.author.name, discrim=ctx.author.discriminator), icon_url=ctx.author.avatar.url)
@@ -123,16 +123,16 @@ class SnedHelp(commands.HelpCommand):
         elif group.help:
             group_embed.add_field(name=self._("Description:"), value=group.help)  #Fallback to help attribute if description does not exist
         if group.usage:
-            group_embed.add_field(name=self._("Usage:"), value=f"**`{self.clean_prefix}{group.usage}`**", inline=False) #Getting command usage & formatting it
+            group_embed.add_field(name=self._("Usage:"), value=f"**`{ctx.clean_prefix}{group.usage}`**", inline=False) #Getting command usage & formatting it
         aliases = []
         for alias in group.aliases:
-            aliases.append(f"**`{self.clean_prefix}{alias}`**")  #Adding some custom formatting to each alias
+            aliases.append(f"**`{ctx.clean_prefix}{alias}`**")  #Adding some custom formatting to each alias
         if aliases:
             group_embed.add_field(name=self._("Aliases:"), value=", ".join(aliases), inline=False)   #If any aliases exist, we add those to the embed in new field
         sub_cmds = []
         filtered = await self.filter_commands(group.walk_commands(), sort=True)
         for command in filtered:
-            sub_cmds.append(self.get_subcommand_signature(group, command))
+            sub_cmds.append(self.get_subcommand_signature(ctx, group, command))
         if sub_cmds:
             sub_cmds = "\n".join(sub_cmds)
             group_embed.add_field(name=self._("Sub-commands:"), value=f"{sub_cmds}")
