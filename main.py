c3215d47d4b54b1bfd73b0f3d37fbd51f08c006c
HyperGH <46067571+HyperGH@users.noreply.github.com>
2021-08-16 17:04:18 +0200

    Add contact link to error

--- main.py
+++ main.py
@@ -350,7 +350,7 @@ class SnedBot(commands.Bot):
                 logging.error(f"Failed to log to server: {error}")
             logging.error(exception_msg)
 
-            embed=discord.Embed(title="❌ " + _("Unhandled exception"), description=_("An error happened that should not have happened. Please contact us with a screenshot of this message!\n**Error:** ```{error}```").format(error=error), color=self.errorColor)
+            embed=discord.Embed(title="❌ " + _("Unhandled exception"), description=_("An error happened that should not have happened. Please [contact us](https://discord.gg/KNKr8FPmJa) with a screenshot of this message!\n**Error:** ```{error}```").format(error=error), color=self.errorColor)
             embed.set_footer(text=f"Guild: {ctx.guild.id}")
             return await ctx.send(embed=embed)
 
